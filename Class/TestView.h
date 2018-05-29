e6539173015b5d2b5336023680e22f99953e8ee0
muzili <liwenrong525@163.com>
2018-05-29 14:55:36 +0800

    liwenorng

---
+++ Class/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  Test
+//
+//  Created by 李文荣 on 2018/5/29.
+//  Copyright © 2018年 李文荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ Class/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  Test
+//
+//  Created by 李文荣 on 2018/5/29.
+//  Copyright © 2018年 李文荣. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
