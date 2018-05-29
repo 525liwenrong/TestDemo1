04631fc66f6b34098387939a6969f8f3454e09be
muzili <liwenrong525@163.com>
2018-05-29 14:30:38 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 李文荣 on 2018/5/29.
+//  Copyright © 2018年 李文荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
